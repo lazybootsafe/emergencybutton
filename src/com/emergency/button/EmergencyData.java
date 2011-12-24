939c4dddf69fd95c90a27fc50f2005872bb011cc
ubershmekel <ubershmekel@localhost>
2011-12-24 20:26:22 +0000

    Now you can have empty emails and phone numbers though I need to make sure they work when you send to them.

--- src/com/emergency/button/EmergencyData.java
+++ src/com/emergency/button/EmergencyData.java
@@ -9,39 +9,54 @@ import android.content.SharedPreferences;
 
 public class EmergencyData {
 	
-	Context context;
-	SharedPreferences settings;
+	Context mContext;
+	SharedPreferences mSettings;
 
 	public static final String PREFS_NAME = "EmergencyPrefsFile";
 	public static final String EMAIL = "emailAddress";
 	public static final String PHONE = "phoneNo";
 	public static final String MESSAGE = "message";
 	public static final String SEND_EMERGENCY = "sendEmergency";
+	
+	private static final String COUNT_SUFFIX = "Count";
 
 	public EmergencyData(Context context) {
-		this.context = context;
-		this.settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+		this.mContext = context;
+		this.mSettings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
 	}
 
+	private int getCount(String id) {
+		return mSettings.getInt(id + COUNT_SUFFIX, 1);
+	}
+	
+	private void setCount(SharedPreferences.Editor editor, String id, int count) {
+		editor.putInt(id + COUNT_SUFFIX, count);
+	}
+	
 	private List<String> getList(String id) {
+		// NOTE: this function should always return a list with at least one
+		//		string.
 		List<String> stringsList = new ArrayList<String>();
 		String lastString;
-		lastString = settings.getString(id, "");
-		int i = 1;
-		while (lastString != "") {
-			stringsList.add(lastString);
+		lastString = mSettings.getString(id, "");
+		stringsList.add(lastString);
+		
+		int stringsCount = getCount(id);
+		for(int i = 1; i < stringsCount; i++) {
 			// NOTE: the first item is just "id" and then next is "id1"
 			//		this is for backwards compatibility, oh well...
-			lastString = settings.getString(id + i, "");
-			i += 1;
+			lastString = mSettings.getString(id + i, "");
+			stringsList.add(lastString);
 		}
 		
 		return stringsList;
 	}
 	
 	private void commitList(String id, List<String> stringList) {
-		SharedPreferences.Editor editor = settings.edit();
-		for (int i = 0; i < stringList.size(); i++) {
+		SharedPreferences.Editor editor = mSettings.edit();
+		int previousCount = getCount(id);
+		int i = 0;
+		for (; i < stringList.size(); i++) {
 			String idName;
 			if (i > 0) {
 				idName = id + i;
@@ -51,9 +66,13 @@ public class EmergencyData {
 			editor.putString(idName, stringList.get(i));
 		}
 		
-		if (stringList.size() > 0) {
-			editor.commit();
+		for(; i < previousCount; i++) {
+			// remove older irrelevant strings
+			editor.remove(id + i);
 		}
+		
+		setCount(editor, id, stringList.size());
+		editor.commit();
 	}
 	
 	public List<String> getEmails() {
@@ -63,16 +82,16 @@ public class EmergencyData {
 		return getList(PHONE);
 	}
 	public String getMessage() {
-		return settings.getString(MESSAGE, "");
+		return mSettings.getString(MESSAGE, "");
 	}
 	public boolean getArmEmergency() {
-		return settings.getBoolean(SEND_EMERGENCY, false);
+		return mSettings.getBoolean(SEND_EMERGENCY, false);
 	}
 	
 	private void commitString(String id, String value) {
 		// We need an Editor object to make preference changes.
 		// All objects are from android.context.Context
-		SharedPreferences.Editor editor = settings.edit();
+		SharedPreferences.Editor editor = mSettings.edit();
 		editor.putString(id, value);
 		editor.commit();
 	}		
@@ -90,7 +109,7 @@ public class EmergencyData {
 	public void setArmEmergency(boolean sendMessage) {
 		// We need an Editor object to make preference changes.
 		// All objects are from android.context.Context
-		SharedPreferences.Editor editor = settings.edit();
+		SharedPreferences.Editor editor = mSettings.edit();
 		editor.putBoolean(SEND_EMERGENCY, sendMessage);
 		editor.commit();
 	}		
